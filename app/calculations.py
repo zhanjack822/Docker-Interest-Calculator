120d5f9dc12cbcd236bc6d8c1ea0ba74c95a0d12
= <kh4zhang@uwaterloo.ca>
2025-06-11 17:33:15 -0400

    docs: changed the parameter names
    `annual_rate` changed to `comp_rate`

--- app/calculations.py
+++ app/calculations.py
@@ -1,11 +1,11 @@
-def calculate_loan_payment(principal: float, annual_rate: float, years: float) -> tuple[float, float, float]:
+def calculate_loan_payment(principal: float, annual_rate: float, years: int) -> tuple[float, float, float]:
     """
     Calculate the monthly payment, total amount paid, and total interest payments on a loan with a given principal,
     annual interest rate, and payment period in years.
 
     :param float principal: Principal loan amount
     :param float annual_rate: annual interest rate as a percentage
-    :param years: loan period in years
+    :param int years: loan period in years
     :return: tuple containing monthly payment, total amount paid, and total interest paid in that order
     """
 
@@ -19,20 +19,20 @@ def calculate_loan_payment(principal: float, annual_rate: float, years: float) -
     total_interest = total_paid - principal
     return monthly_payment, total_paid, total_interest
 
-def calculate_savings_future_value(initial: float, monthly: float, annual_rate: float, years: float) -> float:
+def calculate_savings_future_value(initial: float, monthly: float, comp_rate: float, years: int) -> float:
     """
     Calculate total savings in a savings account with some initial amount saved, a fixed monthly contributions, some
     fixed annual compounding rate, and the savings period.
 
     :param float initial: initial savings amount
     :param float monthly: monthly contribution
-    :param float annual_rate: annual compounding rate as a percentage
-    :param float years: savings period in years
+    :param float comp_rate: compounding rate as a percentage
+    :param int years: savings period in years
     :return: total value of savings at the end of the savings period
     """
 
     months = years * 12
-    monthly_rate = annual_rate / 100 / 12
+    monthly_rate = comp_rate / 100 / 12
     future_value = initial * (1 + monthly_rate) ** months
     for m in range(months):
         future_value += monthly * (1 + monthly_rate) ** (months - m - 1)
