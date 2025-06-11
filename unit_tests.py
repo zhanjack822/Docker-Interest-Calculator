fc82caa537af62e3a35e9eef65519100f6c325c3
= <kh4zhang@uwaterloo.ca>
2025-06-11 03:05:12 -0400

    refactor: expected values are calculated explicitly

--- unit_tests.py
+++ unit_tests.py
@@ -3,35 +3,73 @@ from app.calculations import calculate_loan_payment, calculate_savings_future_va
 
 class TestCalculations(unittest.TestCase):
     def test_loan_regular(self):
-        monthly_payments, total_payment, total_interest = calculate_loan_payment(10000, 5, 5)
-        self.assertAlmostEqual(monthly_payments, 188.71, places=2)
-        self.assertAlmostEqual(total_payment, 11322.74, places=2)
-        self.assertAlmostEqual(total_interest, 1322.74, places=2)
+        # calculate correct interest
+        principal = 10000
+        years = 5
+        months = years * 12
+        annual = 5
+        monthly = (annual / 100) / 12
+        correct_monthly_payment = principal * monthly / (1 - (1 + monthly) ** -months)
+        correct_total_payment = correct_monthly_payment * months
+        correct_total_interest = correct_total_payment - principal
+
+        # obtain our function's output
+        monthly_payments, total_payment, total_interest = calculate_loan_payment(principal, annual, years)
+        self.assertAlmostEqual(monthly_payments, correct_monthly_payment, places=2)
+        self.assertAlmostEqual(total_payment, correct_total_payment, places=2)
+        self.assertAlmostEqual(total_interest, correct_total_interest, places=2)
 
     def test_loan_zero_interest(self):
-        monthly_payments, total_payment, total_interest = calculate_loan_payment(12000, 0, 4)
-        self.assertEqual(monthly_payments, 250.0)
-        self.assertEqual(total_payment, 12000.0)
-        self.assertEqual(total_interest, 0.0)
+
+
+        # calculate correct interest
+        principal = 12000
+        years = 4
+        months = years * 12
+        annual = 0
+        correct_monthly_payment = principal / months
+        correct_total_payment = correct_monthly_payment * months
+        correct_total_interest = 0
+
+        # obtain our function's output
+        monthly_payments, total_payment, total_interest = calculate_loan_payment(principal, annual, years)
+        self.assertAlmostEqual(monthly_payments, correct_monthly_payment, places=2)
+        self.assertAlmostEqual(total_payment, correct_total_payment, places=2)
+        self.assertAlmostEqual(total_interest, correct_total_interest, places=2)
 
     def test_savings_regular(self):
-        calculated_savings = calculate_savings_future_value(1000, 100, 5, 10)
-
         # correct savings calculation
         initial = 1000
         monthly = 100
-        months = 120
-        interest = (5 / 100) / 12
+        years = 10
+        months = years * 12
+        annual = 5
+        interest = (annual / 100) / 12
 
         final = initial * (1 + interest) ** months
         for m in range(months):
             final += monthly * (1 + interest) ** (months - m - 1)
 
+        calculated_savings = calculate_savings_future_value(initial, monthly, annual, years)
         self.assertAlmostEqual(calculated_savings, final, places=2)
 
     def test_savings_zero_rate(self):
-        fv = calculate_savings_future_value(1000, 100, 0, 5)
-        self.assertEqual(fv, 1000 + 100 * 12 * 5)
+        # correct savings calculation
+        initial = 1000
+        monthly = 100
+        years = 5
+        months = years * 12
+        annual = 0
+        interest = (annual / 100) / 12
+
+        final = initial * (1 + interest) ** months
+        for m in range(months):
+            final += monthly * (1 + interest) ** (months - m - 1)
+
+        calculated_savings = calculate_savings_future_value(initial, monthly, annual, years)
+        self.assertAlmostEqual(calculated_savings, final, places=2)
+
 
 if __name__ == '__main__':
     unittest.main()
+
